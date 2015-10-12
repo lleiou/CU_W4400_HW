13b6282a3768251e73ce7a2fc9be6dca8210fb3f
lleiou <lleiou@users.noreply.github.com>
2015-10-12 10:50:16 -0400

    Update classify(S,z).R

--- HW2/Q2/classify(S,z).R
+++ HW2/Q2/classify(S,z).R
@@ -1,1 +1,16 @@
+classify<-function(S,z){
+	
+	n<-length(S[,1])
+    fy<-c(1:n)
+    #fy is used to store the estimate label of every data point 
+    for(i in 1:n){
+    	fy[i]<-sign(z%*%S[i,])
+    	if(fy[i]==0)
+    	{
+    	  fy[i]<-1
+    	}
+    	}	
+    fy
+	
+}
 
