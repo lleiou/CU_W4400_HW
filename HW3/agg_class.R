a3fab7528576a3b578f48be354808c2cd1a18b99
lleiou <lleiou@users.noreply.github.com>
2015-10-26 18:10:43 -0400

    Update agg_class.R

--- HW3/agg_class.R
+++ HW3/agg_class.R
@@ -22,4 +22,52 @@ agg_class<-function(X,alpha,allPars){
   return(as.vector(result))
   
   
+}agg_class<-function(X,alpha,allPars){
+  
+ 
+
+  n<-length(X[,1])
+  #RESULT IS USED TO STORE THE CLASSIFY RESULT IN X
+  result<-rep(0,n)
+  
+  
+  for(i in 1:n){
+    sum<-0
+    
+    for(j in 1:length(allPars[,1])){
+    
+    sum<-sum+alpha[j]*classify(X[i,],allPars[j,])
+    
+    }
+  
+ result[i]<-sign(sum) 
+  }
+  
+  return(as.vector(result))
+  
+  
+}agg_class<-function(X,alpha,allPars){
+  
+ 
+
+  n<-length(X[,1])
+  #RESULT IS USED TO STORE THE CLASSIFY RESULT IN X
+  result<-rep(0,n)
+  
+  
+  for(i in 1:n){
+    sum<-0
+    
+    for(j in 1:length(allPars[,1])){
+    
+    sum<-sum+alpha[j]*classify(X[i,],allPars[j,])
+    
+    }
+  
+ result[i]<-sign(sum) 
+  }
+  
+  return(as.vector(result))
+  
+  
 }
